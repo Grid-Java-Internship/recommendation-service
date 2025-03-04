1cf7a7eef402af99c971d2a6b9cf45b34a9022e2
matijaaz <kreckoviccc2001@gmail.com>
2025-03-04 14:38:00 +0100

    recommendation-servic setup

---
+++ src/main/java/com/internship/recommendation_service/exception/RecommendationExceptionHandler.java
@@ -0,0 +1,7 @@
+package com.internship.recommendation_service.exception;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+
+@ControllerAdvice
+public class RecommendationExceptionHandler {
+}
