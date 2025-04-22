4030ff56690ef9585f3765356cc1b6aef1c865df
Jovan Vukic <jovanv0803@gmail.com>
2025-04-22 15:42:44 +0200

    Fix: Exception handling

---
+++ src/main/java/com/internship/recommendation_service/exception/ServiceUnavailableException.java
@@ -0,0 +1,7 @@
+package com.internship.recommendation_service.exception;
+
+public class ServiceUnavailableException extends RuntimeException {
+    public ServiceUnavailableException(String message) {
+        super(message);
+    }
+}
