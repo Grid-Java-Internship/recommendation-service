bffa816516efa50d5531ab796ca41f8a32afa8e0
Jovan Vukic <jovanv0803@gmail.com>
2025-04-22 15:43:08 +0200

    Add separate logging class

---
+++ src/main/java/com/internship/recommendation_service/util/LogUtil.java
@@ -0,0 +1,38 @@
+package com.internship.recommendation_service.util;
+
+import lombok.experimental.UtilityClass;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@UtilityClass
+public class LogUtil {
+    /**
+     * Logs an INFO message with the given format string and arguments.
+     *
+     * @param message the message to be logged
+     * @param args    the arguments to be used in the formatting
+     */
+    public void info(String message, Object... args) {
+        log.info(message, args);
+    }
+
+    /**
+     * Logs a WARN message with the given format string and arguments.
+     *
+     * @param message the message to be logged
+     * @param args    the arguments to be used in the formatting
+     */
+    public static void warn(String message, Object... args) {
+        log.warn(message, args);
+    }
+
+    /**
+     * Logs an ERROR message with the given format string and arguments.
+     *
+     * @param message the message to be logged
+     * @param args    the arguments to be used in the formatting
+     */
+    public void error(String message, Object... args) {
+        log.error(message, args);
+    }
+}
